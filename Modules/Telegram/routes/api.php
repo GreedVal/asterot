d9928426472d91cc09023d01e171b99779949382
GreedVal <137034289+GreedVal@users.noreply.github.com>
2025-09-25 13:06:55 +0300

    greedval

--- Modules/Telegram/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Telegram\Http\Controllers\TelegramController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('telegrams', TelegramController::class)->names('telegram');
-});
--- Modules/Telegram/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Telegram\Http\Controllers\TelegramController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('telegrams', TelegramController::class)->names('telegram');
-});
